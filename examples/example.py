178ac063fecf20823d2fd9014a42478ba4fca969
Holmes Conan <holmesconan@gmail.com>
2019-05-05 14:05:51 +0800

    New package structure.

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from _ctpclient import CtpClient
+from ctpclient import CtpClient
 
 
 class Client(CtpClient):
--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 
-from setuptools import setup, find_packages
-
-VERSION = (0, 0, 1, 'alpha', 0)
+from setuptools import setup, find_packages, Extension
 
 f = open('README.md', 'r')
 LONG_DESCRIPTION = f.read()
 f.close()
 
+ctpclient_ext = Extension('pkg.ctpclient',
+                          sources=[
+                            'src/ctpclient_ext/binding.cc',
+                            'src/ctpclient_ext/ctpclient.cc',
+                            'src/ctpclient_ext//mdspi.cc',
+                            'src/ctpclient_ext//traderspi.cc'
+                          ],
+                          include_dirs=['/usr/local/include'],
+                          library_dirs=['/usr/local/lib/boost'],
+                          runtime_library_dirs=['/usr/local/lib/boost'],
+                          libraries=['boost_python'])
+
 setup(
     name='pyctpclient',
-    version=VERSION,
+    version='0.0.1a0',
     description='CTP Python client.',
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
     author='Holmes Conan',
     author_email='holmesconan@gmail.com',
-    url='https://github.com/holmescn/ctp.py/',
+    url='https://github.com/holmescn/pyctpclient/',
     license='Apache-2.0',
-    packages=find_packages(exclude=['ez_setup', 'tests*']),
-    package_data={'ctp': ['templates/*']},
-    include_package_data=True,
-    entry_points="""
-        [console_scripts]
-        ctp = ctp.main:main
-    """,
+    package_dir={'': 'src'},
+    packages=['pyctpclient'],
+    ext_modules=[ctpclient_ext],
 )
